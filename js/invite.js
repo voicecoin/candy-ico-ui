1792c326f6449e29aee1e129e29cc68493cd275e
Yuancheng Zhang <old.far.hot@gmail.com>
2018-07-10 10:50:14 +0800

    实现“复制”按钮

--- js/invite.js
+++ js/invite.js
@@ -4,3 +4,14 @@ $(function () {
 	var invite_code = url.searchParams.get('code');
 	$('#candy-invite-code').val(invite_code);
 });
+
+function copyToClipboard() {
+	/* Get the text field */
+	var copyText = document.getElementById("candy-invite-code");
+
+	/* Select the text field */
+	copyText.select();
+
+	/* Copy the text inside the text field */
+	document.execCommand("copy");
+}
